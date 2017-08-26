fc590cf9f24457efc25dbcd069b77995652ca867
shradha15 <31278289+shradha15@users.noreply.github.com>
2017-08-26 15:39:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    //articeName = article-one
-    //articles[articleName] = {} content object for article-one
+    //articeName == article-one
+    //articles[articleName] == {} content object for article-one
     var articleName = req.parans.articleName;
  res.send(createTemplate(articles[articleName]));
 });
